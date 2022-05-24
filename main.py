fd23e66b9608883f0b558ebdcd5338f280cfcd12
NKD <kdunggttn@gmail.com>
2022-05-24 11:24:39 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# import something
+
+def main(data, context):
+    # Insert your code here
+    # GCF yêu cầu function chính phải có param là "data" và "context" kể cả trong code không dùng. Để nguyên 2 param này.
+    return 'Insert your code here'
+
+if __name__ == "__main__":
+    # Block này để test code ở local.
+    main(data=None, context=None)
